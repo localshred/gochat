25cfa3354e87f80b6d8a01f9971f2e5cf1313dcc
BJ Neilsen <bj.neilsen@gmail.com>
2018-10-11 22:01:28 -0400

    Start a basic telnet echo server with given host/port

--- server.go
+++ server.go
@@ -1,7 +1,27 @@
 package main
 
+import (
+	"fmt"
+	"os"
+
+	telnet "github.com/reiver/go-telnet"
+)
 func main() {
 	config := readConfig()
+	handler := telnet.EchoHandler
+	server := &telnet.Server{
+		Addr:    fmt.Sprintf("%s:%v", config.Host, config.Port),
+		Handler: handler,
+	}
+
+	err := server.ListenAndServe()
+	if nil != err {
+		fmt.Fprintln(os.Stderr, "Unable to start server: ", err)
+		os.Exit(-1)
+	}
+	fmt.Printf("Server listening on %v", config.Port)
+}
+
 type chatServerConfig struct {
 	Host    string
 	Port    int
