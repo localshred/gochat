102a3d3018aa4e4faf69cb6f852b530f535f7a58
BJ Neilsen <bj.neilsen@gmail.com>
2018-10-11 22:00:34 -0400

    Get a default config

--- server.go
+++ server.go
@@ -1,4 +1,18 @@
 package main
 
 func main() {
+	config := readConfig()
+type chatServerConfig struct {
+	Host    string
+	Port    int
+	LogFile string
+}
+
+func readConfig() *chatServerConfig {
+	defaultConfig := &chatServerConfig{
+		Host:    "localHost",
+		LogFile: "logs/server.log",
+		Port:    5555,
+	}
+	return defaultConfig
 }
