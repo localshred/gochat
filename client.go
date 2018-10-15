f190653ad086cc5504450672cc38bc6de78c73e8
BJ Neilsen <bj.neilsen@gmail.com>
2018-10-15 15:30:27 -0400

    Read and write through a buffered reader/writer

--- client.go
+++ client.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"net"
 )
 
@@ -17,8 +19,8 @@ _____  ____   _____ _    _       _______
 \_____|\____/ \_____|_|  |_/_/    \_|_|
 
 Type any message you like and hit <return> to send to the channel.
-Type help<return> for a list of commands.
-Type exit<return> to leave chat.
+Type /help<return> for a list of commands.
+Type /exit<return> to leave chat.
 
 `
 )
@@ -29,14 +31,32 @@ type Client struct {
 	Channel *Channel
 	Conn    net.Conn
 	Context *Context
+	Scanner *bufio.Scanner
 	User    *User
+	Writer  *bufio.Writer
 }
 
 func (client *Client) connected() {
-	client.Conn.Write([]byte(welcomeMessage))
 	client.Context.Logger.Debugf("Client connected from %s", client.Conn.RemoteAddr())
+	client.Writer = bufio.NewWriter(client.Conn)
+	client.Scanner = bufio.NewScanner(client.Conn)
+	client.Scanner.Split(bufio.ScanLines)
+
+	client.writeString(welcomeMessage)
 }
 
 func (client *Client) listen() {
+func (client *Client) writeLine(line string) (err error) {
+	return client.writeString(fmt.Sprintf("%s\n", line))
+}
 
+
+func (client *Client) writeString(line string) (err error) {
+	if _, err = client.Writer.WriteString(line); nil != err {
+		return err
+	}
+	if err = client.Writer.Flush(); nil != err {
+		return err
+	}
+	return nil
 }
