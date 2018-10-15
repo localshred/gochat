46959acb20b1139bd33bcc896d68b7786589c9d8
BJ Neilsen <bj.neilsen@gmail.com>
2018-10-15 11:35:09 -0400

    Get rid of telnet/telsh dependency for greater client connection control

--- telnet-server.go
+++ telnet-server.go
@@ -2,33 +2,61 @@ package main
 
 import (
 	"fmt"
-
-	telnet "github.com/reiver/go-telnet"
+	"net"
 )
 
 // TelnetServer represents a wrapped telnet server
 type TelnetServer struct {
-	Config *chatServerConfig
+	Clients  []*Client
+	Channels []*Channel
+	Config   *chatServerConfig
+	Logger   *telnetLogger
 }
 
 func (telnetServer *TelnetServer) start() {
 	config := telnetServer.Config
 	address := fmt.Sprintf("%s:%v", config.Host, config.Port)
-	handler := telnet.EchoHandler
 	logger, logFile, err := createLogger(config.LogFile)
 	if err != nil {
 		printErrorAndExit(err, -1)
 	}
 	defer logFile.Close()
+	telnetServer.Logger = logger
 
-	server := &telnet.Server{
-		Addr:    address,
-		Handler: handler,
-		Logger:  logger,
-	}
-
-	fmt.Printf("Server listening on %s\n", address)
-	if err := server.ListenAndServe(); err != nil {
+	listener, err := net.Listen("tcp", address)
+	if nil != err {
 		printErrorAndExit(err, -1)
 	}
+	defer listener.Close()
+
+	listeningMessage := fmt.Sprintf("Server listening on %s\n", address)
+	fmt.Print(listeningMessage)
+	telnetServer.Logger.Debug(listeningMessage)
+	for {
+		telnetServer.acceptConnection(listener)
+	}
+}
+
+func (telnetServer *TelnetServer) acceptConnection(listener net.Listener) {
+	conn, err := listener.Accept()
+	if nil != err {
+		printErrorAndExit(err, -1)
+	}
+	defer func() {
+		telnetServer.Logger.Debugf("Client disconnected from %s", conn.RemoteAddr())
+		conn.Close()
+	}()
+
+	telnetServer.Logger.Debugf("Client connected from %s", conn.RemoteAddr())
+
+	client := &Client{
+		Channel: &Channel{"general"},
+		Conn:    conn,
+		User:    &User{"anonymous"},
+	}
+	telnetServer.Clients = append(telnetServer.Clients, client)
+
+	client.connected()
+	go client.listen()
+	// TODO spawn go-routine to read client messages
 }
